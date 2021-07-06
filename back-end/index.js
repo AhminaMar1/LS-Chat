b4088c95d568fb9d9d60270995ab648344e26922
AHMINA Marouan <AhminaMar1@gmail.com>
2021-07-06 11:39:01 +0100

    Add socket.io in the server-side

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http')
+const socketIo = require('socket.io')
 const cors = require('cors')
 
 //The env
@@ -10,6 +11,8 @@ const app = express()
 const server = http.createServer(app)
 
 const port = process.env.PORT || 5000;
+const clientURL = process.env.CLIENT_SIDE;
+
 
 //Cors middleware 
 app.use(cors())
@@ -20,6 +23,23 @@ app.get('/' , (req , res)=>{
 
    res.status(200).send('The index http runing');
 
+});
+
+//Init socket.io
+const io = socketIo(server, {
+   cors: {
+       origin: clientURL,
+   }
+});
+
+io.on("connection", (socket) => {
+   console.log("New client connected");
+
+   socket.emit('FromAPI', {socket: true})
+
+   socket.on("disconnect", () => {
+      console.log("Client disconnected");
+   });
 });
 
 

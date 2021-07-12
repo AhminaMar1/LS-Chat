b52284ac8a5721727f501cb529fc8a0b462859df
AHMINA Marouan <AhminaMar1@gmail.com>
2021-07-12 22:13:37 +0100

    Change start-sessopm functoion  to pure function

--- server-side/src/functions/client.js
+++ server-side/src/functions/client.js
@@ -1,22 +1,22 @@
 ///// start session - client
-// random name
-const names = ['Chai', 'Robin', 'Lofi', 'Soku', 'Yori', 'Ham'];
-const leng = names.length;
-
-const randomName = () => {
-    let randIndex = Math.floor(Math.random()* leng)
-    
-    return names[randIndex] + Math.floor(Math.random()* 1000);
-}
-// random token
-const randChars = () => {
-    return Math.random().toString(36).substr(2);
-};
-
-const token = () => {
-    return randChars() + randChars(); // to make it longer
-};
 exports.saveNewSession = async (User, res) => {
+    // random name
+    const names = ['Chai', 'Robin', 'Lofi', 'Soku', 'Yori', 'Ham'];
+    const leng = names.length;
+    
+    const randomName = () => {
+        let randIndex = Math.floor(Math.random()* leng)
+        
+        return names[randIndex] + Math.floor(Math.random()* 1000);
+    }
+    // random token
+    const randChars = () => {
+        return Math.random().toString(36).substr(2);
+    };
+    
+    const token = () => {
+        return randChars() + randChars(); // to make it longer
+    };
     
     //Create new session - new user
     const user = new User({

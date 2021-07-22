40a001a4bd6e120e20baed818f82963d6c5363e8
AHMINA Marouan <AhminaMar1@gmail.com>
2021-07-22 14:18:52 +0100

    bcrypt of password - we used bcrypt library

--- server-side/src/controllers/admin.js
+++ server-side/src/controllers/admin.js
@@ -1,4 +1,6 @@
 const Admin = require('../models/admin')
+const bcrypt = require ('bcrypt');
+
 
 exports.firstGet= (req, res) => {
 
@@ -11,24 +13,36 @@ exports.moreConversations= (req, res) => {
 
 exports.login= async (req, res) => {
 
+    const infWrong = (res) => {
+        let result = {
+            success_login: false,
+        }
+        res.status(200).json(result)
+    }
+
     let username = req.body.username || "test",
     password = req.body.password
     Admin.findOne({
         "username": username
     }, (err, data) => {
-        if(err || data==null || password !== data.password) {
-            let result = {
-                success_login: false,
-            }
-            res.status(200).json(result)
-        } else {
-            let result = {
-                success_login: true,
-                id: data._id,
-                token: data.token
-            }
-            res.status(200).json(result)
-        }
-    })
 
+        if(err || data==null || data.password == null){
+            infWrong(res);
+        }else{
+
+            bcrypt.compare(password, data.password, (err, checkPassword) => {
+
+                if(checkPassword) {
+                    let result = {
+                        success_login: true,
+                        id: data._id,
+                        token: data.token
+                    }
+                    res.status(200).json(result)
+                }else {
+                    infWrong(res);
+                }
+            });
+        }
+    });
 }

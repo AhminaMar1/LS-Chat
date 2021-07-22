0c25873362444f0cfd420c7f6fae1e319ee74999
AHMINA Marouan <AhminaMar1@gmail.com>
2021-07-22 15:41:33 +0100

    Add possibilty to change type of flash message to: warningFlashMessage - errorFlashMessage -succ...

--- client-side/src/mainComponents/admin/components/FlashMessage.js
+++ client-side/src/mainComponents/admin/components/FlashMessage.js
@@ -1,15 +1,32 @@
 import React, {useState, useEffect} from 'react'
 
-export default function FlashMessage({message, toHid, newToHidState}) {
+export default function FlashMessage({type, message, toHid, newToHidState}) {
     
-    
+    //styles
+    const warningStyle = {
+        backgroundColor: '#ffeb85',
+    }
+    const successStyle = {
+        backgroundColor: '#dff2c1',
+    }
+    const errorStyle = {
+        backgroundColor: '#f7baba',
+    }
+    const infStyle = {
+        backgroundColor: '#f7baba',
+    }
+
+    //states
     const [counter, setCounter] = useState(7);
 
+    //setInterval (id)
     const interval = () => {
         setInterval(() => {
             setCounter(c => c-1)
         }, 1000)
     }
+
+    // use effects
     useEffect(() => {
 
         interval();
@@ -22,9 +39,14 @@ export default function FlashMessage({message, toHid, newToHidState}) {
             toHid(newToHidState)
         }
     }, [counter, toHid, newToHidState]);
-    
+
     return (
-        <div className="contianer-flash-message">
+        <div className="contianer-flash-message"
+        style={
+            type==='succ' ? successStyle
+            :type==='inf' ? infStyle
+            :type==='error' ? errorStyle
+            : warningStyle}>
             <div className="close"><span>×</span></div>
             <div className="counter">{counter}</div>
             <p>{message}</p>
